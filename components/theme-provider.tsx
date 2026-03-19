b8923ac2e50fc5b4062a0825c90286a1bbb30679
hoachnt <hoachprogrammer@gmail.com>
2026-03-19 11:35:42 +0700

    Add transition when changing theme

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,7 +5,36 @@ import {
 	ThemeProvider as NextThemesProvider,
 	type ThemeProviderProps,
 } from "next-themes";
+import { useTheme } from "next-themes";
+
+function ThemeTransitionController() {
+	const { resolvedTheme } = useTheme();
+	const hasMountedRef = React.useRef(false);
+
+	React.useEffect(() => {
+		if (!hasMountedRef.current) {
+			hasMountedRef.current = true;
+			return;
+		}
+
+		const root = document.documentElement;
+		root.classList.add("theme-transition");
+
+		const timeoutId = window.setTimeout(() => {
+			root.classList.remove("theme-transition");
+		}, 250);
+
+		return () => window.clearTimeout(timeoutId);
+	}, [resolvedTheme]);
+
+	return null;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+	return (
+		<NextThemesProvider {...props}>
+			<ThemeTransitionController />
+			{children}
+		</NextThemesProvider>
+	);
 }
